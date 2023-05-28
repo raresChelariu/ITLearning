74f95cc14902b8de4bf5eed4880b9c381b4e7543
Rares <rareschelariu79@gmail.com>
2023-05-28 22:32:00 +0300

    Add Course Controller

---
+++ ITLearningAPI.Web/Controllers/Ui/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using ITLearningAPI.Web.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITLearningAPI.Web.Controllers.Ui;
+
+[ApiController]
+[Route("[controller]")]
+public class CourseController : ControllerBase
+{
+    private readonly IStaticAssetResponseService _staticAssetResponseService;
+
+    public CourseController(IStaticAssetResponseService staticAssetResponseService)
+    {
+        _staticAssetResponseService = staticAssetResponseService 
+                                      ?? throw new ArgumentNullException(nameof(staticAssetResponseService));
+    }
+
+    [HttpGet]
+    [Route("create")]
+    public async Task GetCourseCreate()
+    {
+        await _staticAssetResponseService.RespondWithStaticAsset(Response, "CourseCreate.html");
+    }
+}
