c145da65fcbe70056f7b722c28718dad195de96e
Rares <rareschelariu79@gmail.com>
2023-06-14 02:56:16 +0300

    student - show courses as cards for Courses All page

--- ITLearningAPI.Web/Static/js/Student/coursesAll.js
+++ ITLearningAPI.Web/Static/js/Student/coursesAll.js
@@ -1,19 +1,41 @@
-import { FetchHttpGet } from "/js/Fetcher.js";
+import {FetchHttpGet} from "/js/Fetcher.js";
+import {CreateAlertError} from "/js/Alert.js";
 
 const listCourses = document.getElementById("listCourses");
 
-FetchHttpGet("/api/course/all").then(responseResult => {
-    console.log(responseResult);
-    for (let i = 0; i < responseResult.length; i++) {
-        const course = document.createElement("button");
+FetchHttpGet("/api/course/all")
+    .then(responseResult => {
+        listCourses.classList.add("cards");
+        console.log(responseResult);
+        for (let i = 0; i < responseResult.length; i++) {
+            const course = createCourseElement(responseResult[i]);
+            listCourses.appendChild(course);
+        }
+    }).catch(err => {
+        CreateAlertError("Cursurile nu au putut fi aduse de la server!");
+        console.log(err);
+    });
 
-        course.innerText = responseResult[i]["name"];
-        const courseId = responseResult[i]["id"];
-        course.dataset.id = courseId;
-        course.addEventListener("click", () => {
-            window.location.replace(`/course/${courseId}`);
-        });
+function createCourseElement(data) {
+    const courseId = data["id"];
 
-        listCourses.appendChild(course);
-    }
-});
+    const courseTitle = document.createElement("h2");
+    courseTitle.innerText = data["name"];
+
+    const description = document.createElement("p");
+    description.innerText = "Some Course Description Here";
+
+    const container = document.createElement("div")
+    container.classList.add("card-content");
+    container.appendChild(courseTitle);
+    container.appendChild(description);
+
+    const course = document.createElement("article");
+    course.classList.add("card");
+    course.appendChild(container);
+    course.dataset.id = courseId;
+    course.addEventListener("click", () => {
+        window.location.replace(`/course/${courseId}`);
+    });
+    return course;
+}
